2002ce63cfafc12283639e50e26b6d94971d56e4
Amir Sultan <amir.sultan@student.ucll.be>
2025-12-16 11:38:39 +0100

    main.py version from Amir

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 from screens.loading import LoadingScreen
 
-<<<<<<< Updated upstream
-=======
 class State:
     def __init__(self):
         self.x = 0
@@ -13,32 +11,19 @@ class State:
     def render(self, surface):
         pygame.draw.circle(surface, (255, 0, 0), (self.x, 300), 50)
 
->>>>>>> Stashed changes
 def create_main_surface():
     surface = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Alpha Ninja")
     return surface
 
-<<<<<<< Updated upstream
-def render_frame(surface):
-    pygame.draw.circle(
-        surface,
-        (255, 0, 0),
-        (400, 300),
-        50
-    )
-=======
 def render_frame(surface, state):
     surface.fill((0, 0, 0))
     state.render(surface)
->>>>>>> Stashed changes
     pygame.display.flip()
 
 def MAIN():
     pygame.init()
     surface = create_main_surface()
-<<<<<<< Updated upstream
-=======
 
     # ✅ LOADING SCREEN (NO IMAGES)
     loading = LoadingScreen(surface)
@@ -54,7 +39,6 @@ def MAIN():
         loading.render()
 
     # ✅ GAME STARTS
->>>>>>> Stashed changes
 
     while True:
         for event in pygame.event.get():
