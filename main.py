784a71e0490da3ea6033941b7088b82b7e9f7e54
Haseeb <r1077528@ucll.be>
2025-12-16 10:58:59 +0100

    Images

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 import pygame
 
-class State:
-    def __init__(self):
-        self.x = 0
-
-    def update(self):
-        self.x += 1
-
-    def render(self, surface):
-        pygame.draw.circle(
-            surface,
-            (255, 0, 0),
-            (self.x, 300),
-            50
-        )
-
 def create_main_surface():
     WIDTH, HEIGHT = 800, 600
     surface = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("My Game")
     return surface
 
-def render_frame(surface, state):
-    state.render(surface)
+def render_frame(surface):
+    pygame.draw.circle(
+        surface,
+        (255, 0, 0),
+        (400, 300),
+        50
+    )
     pygame.display.flip()
 
 def MAIN():
     pygame.init()
     surface = create_main_surface()
-    state = State()
 
     while True:
         for event in pygame.event.get():
@@ -36,7 +25,6 @@ def MAIN():
                 pygame.quit()
                 exit()
 
-        state.update()
-        render_frame(surface, state)
+        render_frame(surface)
 
 MAIN()
